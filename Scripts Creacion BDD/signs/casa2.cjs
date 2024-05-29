c9f505a048b6f04160738fc60f212f0b12cc1346
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-29 12:42:04 +0200

    categoria 2 de casa terminada

--- Scripts Creacion BDD/signs/casa2.cjs
+++ Scripts Creacion BDD/signs/casa2.cjs
@@ -30,3 +30,185 @@ var data = {
 var documentRef = doc(collectionRef, id.toString());
 
 setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Baño',
+  description: 'Espacio para higiene personal y relajación.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fbano.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Cepillo',
+  description: 'Instrumento para higiene dental y cuidado capilar.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fcepillo.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Champu',
+  description: 'Limpia y nutre cabello, elimina residuos.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fchampu.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Colonia',
+  description: 'Fragancia refrescante, aroma personalizado.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fcolonia.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Espejo',
+  description: 'Refleja imagen, esencial en rutinas diarias.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fespejo.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Esponja',
+  description: 'Absorbe agua, utensilio para limpieza y exfoliación.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fesponja.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Jabon',
+  description: 'Limpia y refresca, esencial en higiene diaria.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fjabon.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Lavabo',
+  description: 'Fuente de agua, para lavado de manos.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Flavabo.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Objetos',
+  description: 'Diversidad de cosas, variados propósitos.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fobjetos.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Papel',
+  description: 'Útil para escritura, limpieza y empaque.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fpapel.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Pasta de dientes',
+  description: 'Limpia y protege dientes, previene caries.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fpasta-de-dientes.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Peine',
+  description: 'Desenreda cabello, para peinados y estilo.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Fpeine.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Toalla',
+  description: 'Absorbe agua, seca cuerpo después del baño.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/casa%2FobjectosDelBano%2Ftoalla.png?alt=media&token=f720155c-871d-4b98-8b84-ebccdb6cfd7a',
+  idCategorie: 15
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
