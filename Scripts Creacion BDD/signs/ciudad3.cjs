f1d6efc206d8d36d39575ed472e62de9b2c1f585
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-29 18:33:10 +0200

    Finalizada categoria 3 de ciudad

--- Scripts Creacion BDD/signs/ciudad3.cjs
+++ Scripts Creacion BDD/signs/ciudad3.cjs
@@ -21,9 +21,93 @@ id++;
 
 var data = {
   id: id,
+  name: 'Profesiones',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fprofesiones.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
   name: 'Bombero',
   description: 'Profesional que combate incendios.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fbombero.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Medico',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fmedico.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Pastelero',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fpastelero.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Pescador',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fpescador.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Policia',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fpolicia.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
+  idCategorie: 25
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Profesor',
+  description: 'Profesional que combate incendios.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/ciudad%2Fprofesiones%2Fprofesor.png?alt=media&token=62717543-b270-4736-a45a-da5e242f8f35',
   idCategorie: 25
 };
 
