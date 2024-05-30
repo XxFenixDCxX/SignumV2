a000c86971d7c69a77040502b32a533e0b1cdd64
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2024-05-30 13:09:14 +0200

    Corregidas descripciones

--- Scripts Creacion BDD/signs/naturaleza3.cjs
+++ Scripts Creacion BDD/signs/naturaleza3.cjs
@@ -36,7 +36,7 @@ id++;
 var data = {
   id: id,
   name: 'Abeja',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Insecto polinizador, vital para el ecosistema.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fabeja.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -50,7 +50,7 @@ id++;
 var data = {
   id: id,
   name: 'Aguila',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Ave majestuosa, símbolo de poder y libertad.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Faguila.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -64,7 +64,7 @@ id++;
 var data = {
   id: id,
   name: 'Ballena',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Mamífero marino gigante, esencial en los océanos.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fballena.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -78,7 +78,7 @@ id++;
 var data = {
   id: id,
   name: 'Burro',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal doméstico resistente, usado para carga.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fburro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -92,7 +92,7 @@ id++;
 var data = {
   id: id,
   name: 'Caballo',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Elegante equino, compañero de trabajo y deporte.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fcaballo.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -106,7 +106,7 @@ id++;
 var data = {
   id: id,
   name: 'Cabra',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal doméstico ágil, productor de leche y carne.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fcabra.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -120,7 +120,7 @@ id++;
 var data = {
   id: id,
   name: 'Camello',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Mamífero desértico, adaptado para transportar carga.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fcamello.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -134,7 +134,7 @@ id++;
 var data = {
   id: id,
   name: 'Cerdo',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal doméstico omnívoro, criado por su carne.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fcerdo.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -148,7 +148,7 @@ id++;
 var data = {
   id: id,
   name: 'Cocodrilo',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Reptil semiacuático, depredador en hábitats acuáticos.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fcocodrilo.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -162,7 +162,7 @@ id++;
 var data = {
   id: id,
   name: 'Conejo',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Pequeño mamífero herbívoro, ágil y prolífico.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fconejo.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -176,7 +176,7 @@ id++;
 var data = {
   id: id,
   name: 'Delfin',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Cetáceo inteligente, ágil y social, habitante de océanos.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fdelfin.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -190,7 +190,7 @@ id++;
 var data = {
   id: id,
   name: 'Elefante',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Majestuoso mamífero terrestre, de gran tamaño y memoria.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Felefante.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -204,7 +204,7 @@ id++;
 var data = {
   id: id,
   name: 'Foca',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Pinnípedo nadador, con aletas y pelaje denso.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ffoca.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -218,7 +218,7 @@ id++;
 var data = {
   id: id,
   name: 'Gallina',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Ave doméstica, ponedora de huevos y carne común.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fgallina.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -232,7 +232,7 @@ id++;
 var data = {
   id: id,
   name: 'Gato',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Felino doméstico, independiente y cazador ágil.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fgato.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -246,7 +246,7 @@ id++;
 var data = {
   id: id,
   name: 'Granja',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Terreno agrícola con animales y cultivos para producción.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fgranja.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -260,7 +260,7 @@ id++;
 var data = {
   id: id,
   name: 'Jirafa',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Mamífero herbívoro de cuello largo, habitante de sabanas.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fjirafa.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -274,7 +274,7 @@ id++;
 var data = {
   id: id,
   name: 'Leon',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Gran felino carnívoro, líder en la sabana africana.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fleon.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -288,7 +288,7 @@ id++;
 var data = {
   id: id,
   name: 'Loro',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Ave exótica, colorida y parlante, popular como mascota.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Floro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -302,7 +302,7 @@ id++;
 var data = {
   id: id,
   name: 'Mariposa',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Insecto volador, colorido y delicado, polinizador.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fmariposa.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -316,7 +316,7 @@ id++;
 var data = {
   id: id,
   name: 'Mono',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Primate ágil, habitante de selvas y bosques tropicales.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fmono.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -330,7 +330,7 @@ id++;
 var data = {
   id: id,
   name: 'Oso',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Mamífero carnívoro, grande y poderoso, habitante de bosques.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Foso.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -344,7 +344,7 @@ id++;
 var data = {
   id: id,
   name: 'Oveja',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal doméstico lanudo, productor de lana y carne.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Foveja.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -358,7 +358,7 @@ id++;
 var data = {
   id: id,
   name: 'Pajaro',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Ave pequeña, voladora y ornamental, con diversidad de especies.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fpajaro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -372,7 +372,7 @@ id++;
 var data = {
   id: id,
   name: 'Pato',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Ave acuática, nadadora y migratoria, de colores llamativos.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fpato.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -386,7 +386,7 @@ id++;
 var data = {
   id: id,
   name: 'Perro',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Canino doméstico, leal y diverso en razas y funciones.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Fperro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -400,7 +400,7 @@ id++;
 var data = {
   id: id,
   name: 'Tiburon',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Pez depredador, temido en los océanos por su ferocidad.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftiburon.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -414,7 +414,7 @@ id++;
 var data = {
   id: id,
   name: 'Tigre',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Gran felino rayado, depredador ágil y poderoso.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftigre.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -428,7 +428,7 @@ id++;
 var data = {
   id: id,
   name: 'Toro',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal robusto, usado en trabajo agrícola y en corridas.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftoro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -442,7 +442,7 @@ id++;
 var data = {
   id: id,
   name: 'Tortuga',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Reptil acuático o terrestre, con caparazón protector.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftoro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -456,7 +456,7 @@ id++;
 var data = {
   id: id,
   name: 'Vaca',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Animal herbívoro, productor de leche y carne, común en granjas.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftoro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
@@ -470,7 +470,7 @@ id++;
 var data = {
   id: id,
   name: 'Zoo',
-  description: 'Creatura de la naturaleza; ser vivo no humano.',
+  description: 'Recinto exhibidor de animales, popular para entretenimiento y educación.',
   imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/naturaleza%2Fanimales%2Ftoro.png?alt=media&token=f7cf6506-5994-430f-8a0d-b695ae6b771e',
   idCategorie: 35
 };
