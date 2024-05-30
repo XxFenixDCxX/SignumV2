d5ad9d19fa23a11fde134f012d9edb78357a4bf8
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2024-05-30 12:24:28 +0200

    calenadrio 5 terminado

--- Scripts Creacion BDD/signs/calendario5.cjs
+++ Scripts Creacion BDD/signs/calendario5.cjs
@@ -30,3 +30,59 @@ var data = {
 var documentRef = doc(collectionRef, id.toString());
 
 setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Cumpleaños',
+  description: 'Aniversario del nacimiento de una persona, celebrado con regalos y fiesta.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/calendario%2Ffiestas%2Fcumpleanos.png?alt=media&token=e5f37178-e815-4f2a-864e-49449124ae14',
+  idCategorie: 31
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Navidad',
+  description: 'Festividad cristiana que celebra el nacimiento de Jesús, con regalos y reuniones familiares.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/calendario%2Ffiestas%2Fnavidad.png?alt=media&token=e5f37178-e815-4f2a-864e-49449124ae14',
+  idCategorie: 31
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Papa Noel',
+  description: 'Figura navideña que entrega regalos a los niños en Nochebuena.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/calendario%2Ffiestas%2Fpapa-noel.png?alt=media&token=e5f37178-e815-4f2a-864e-49449124ae14',
+  idCategorie: 31
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
+
+id++;
+
+var data = {
+  id: id,
+  name: 'Reyes Magos',
+  description: 'Tres sabios que llevaron regalos al niño Jesús el 6 de enero.',
+  imageUrl: 'https://firebasestorage.googleapis.com/v0/b/signumv2.appspot.com/o/calendario%2Ffiestas%2Freyes-magos.png?alt=media&token=e5f37178-e815-4f2a-864e-49449124ae14',
+  idCategorie: 31
+};
+
+var documentRef = doc(collectionRef, id.toString());
+
+setDoc(documentRef, data);
