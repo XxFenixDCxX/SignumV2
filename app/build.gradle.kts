48a6e6f07e0ad78a8d4a7ec8ba3476fd29fbfd12
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-31 21:56:24 +0200

    Integrado firebase en la aplicacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,6 +35,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    //noinspection UseTomlInstead
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    //noinspection UseTomlInstead
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
