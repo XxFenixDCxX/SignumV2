a124487698ab5b10a10a1c22fe6dfb926af6cdb6
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-31 18:07:36 +0200

    Diseño de inicio de sesion y logica para ir a la pantalal de inicio de sesion

--- app/src/main/java/com/fenixdc/signum/LoginActivity.java
+++ app/src/main/java/com/fenixdc/signum/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.fenixdc.signum;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button login;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        login = findViewById(R.id.btnLogin);
-        login.setOnClickListener(v -> {
-            this.finish();
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        });
-
     }
 }
