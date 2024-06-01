4a1e28d8889abcaa434b6c6257fdb996c0c85155
XxFenixDCxX <fenixdc7@gmail.com>
2024-06-01 09:19:02 +0200

    Refactorizado codigo y agregado boton para acceder a pantalla register actualmente vacia

--- app/src/main/java/com/fenixdc/signum/RegisterActivity.java
+++ app/src/main/java/com/fenixdc/signum/RegisterActivity.java
@@ -1,8 +1,6 @@
 package com.fenixdc.signum;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button login, register;
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        login = findViewById(R.id.btnLogin);
-        register = findViewById(R.id.btnRegister);
-
-        login.setOnClickListener(v -> openActivity(LoginActivity.class));
-
-        register.setOnClickListener(v -> openActivity(RegisterActivity.class));
-    }
-    private void openActivity(Class<?> cls) {
-        this.finish();
-        Intent intent = new Intent(this, cls);
-        startActivity(intent);
     }
 }
