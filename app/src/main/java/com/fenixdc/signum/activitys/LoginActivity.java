027d532faedec25e5527a84198eae470a800359b
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2024-09-16 18:32:49 +0200

    Logica de inicio de sesion con google

--- app/src/main/java/com/fenixdc/signum/activitys/LoginActivity.java
+++ app/src/main/java/com/fenixdc/signum/activitys/LoginActivity.java
@@ -16,13 +16,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.fenixdc.signum.R;
 import com.fenixdc.signum.utils.DialogUtils;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    static final int RC_SIGN_IN = 9001;
     TextView register, forgotPassword;
     Button login;
     EditText email, password;
     ImageView google, facebook, twitter;
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +72,7 @@ public class LoginActivity extends AppCompatActivity {
         register.setOnClickListener(v -> openActivity(RegisterActivity.class, false));
         login.setOnClickListener(v -> validateElements());
         forgotPassword.setOnClickListener(v -> sendResetEmail());
+        google.setOnClickListener(v -> googleLogin());
     }
 
     private void openActivity(Class<?> cls, boolean finish) {
@@ -105,5 +124,66 @@ public class LoginActivity extends AppCompatActivity {
                     DialogUtils.showErrorDialog(this, getString(R.string.error), getString(R.string.errorResetEmail));
                 }
             });
+    }
+
+    private void googleLogin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+
+        Intent sigInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(sigInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                DialogUtils.showErrorDialog(this, getString(R.string.error), getString(R.string.erroLogin));
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        registerUser();
+                    } else {
+                        DialogUtils.showErrorDialog(this, getString(R.string.error), getString(R.string.erroLogin));
+                    }
+                });
+    }
+
+
+    private void registerUser() {
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("users");
+        if(mAuth.getCurrentUser() == null || mAuth.getCurrentUser().getEmail() == null || mAuth.getCurrentUser().getDisplayName() == null){
+            DialogUtils.showErrorDialog(this, getString(R.string.error), getString(R.string.erroLogin));
+            return;
+        }
+        String email = mAuth.getCurrentUser().getEmail();
+        String username = mAuth.getCurrentUser().getDisplayName();
+        DocumentReference document = usersCollection.document(email);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("username", username);
+        data.put("email", email);
+
+        document.set(data)
+                .addOnSuccessListener(aVoid -> openActivity(DictionaryActivity.class, true))
+                .addOnFailureListener(e -> DialogUtils.showErrorDialog(this, getString(R.string.error), getString(R.string.erroLogin)));
     }
 }
