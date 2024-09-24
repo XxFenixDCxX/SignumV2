58a7285e2cfe7fb50037c48684cf32f19135f082
XxFenixDCxX <fenixdc7@gmail.com>
2024-09-24 17:19:20 +0200

    Logica de cambiar de cuenta y de cerrar sesion

--- app/src/main/java/com/fenixdc/signum/activitys/profile/ProfileActivity.java
+++ app/src/main/java/com/fenixdc/signum/activitys/profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.fenixdc.signum.activitys.profile;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.fenixdc.signum.R;
+import com.fenixdc.signum.activitys.loginRegister.LoginActivity;
+import com.fenixdc.signum.activitys.loginRegister.MainActivity;
+import com.fenixdc.signum.utils.GeneralUtils;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+    LinearLayout personalDetails, certificate, ranking, changeAccount, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,29 @@ public class ProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        setUpElements();
+        setUpListeners();
+    }
+
+    private void setUpElements(){
+        personalDetails = findViewById(R.id.llPersonalDetails);
+        certificate = findViewById(R.id.llCertificates);
+        ranking = findViewById(R.id.llRanking);
+        changeAccount = findViewById(R.id.llChangeAcount);
+        logout = findViewById(R.id.llCloseSesion);
+    }
+
+    private void setUpListeners(){
+        changeAccount.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            GeneralUtils.openActivity(this, LoginActivity.class, true);
+        });
+
+        logout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            GeneralUtils.openActivity(this, MainActivity.class, true);
         });
     }
 }
